94ad29dd348cb462954416720bc480f8ba4177ec
ranggadsaputra <ranggadsaputra11@gmail.com>
2025-07-10 15:02:10 +0700

    update

---
+++ Navy-Minimalis/script.js
@@ -0,0 +1,8 @@
+// Pastikan ini ada di bawah HTML-mu, sebelum </body>
+const menuBtn = document.querySelector('#menu-btn');
+const navigation = document.querySelector('.navigation');
+
+menuBtn.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+});
